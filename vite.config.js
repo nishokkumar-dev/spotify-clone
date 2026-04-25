1d040e9afef3818e54e9f114ce82e29bd0749408
Nishok Kumar <nishokkumar7ee@gmail.com>
2026-04-25 15:14:04 +0530

    Added PWA support with icons and manifest

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [
+  tailwindcss(),
+  react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Spotify Clone',
+      short_name: 'Spotify',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#121212',
+      theme_color: '#121212',
+     icons: [
+  {
+    src: '/icon-192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: '/icon-512.png',
+    sizes: '512x512',
+    type: 'image/png'
+  }
+]
+    }
+  })
+]
 })
